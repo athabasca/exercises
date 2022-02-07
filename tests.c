daa757267d2442a4f483b47d9e0db36b12af4a9a
athabasca <athabasca@users.noreply.github.com>
2022-02-07 13:45:07 -0800

    Add test runner

---
+++ tests.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include "book_list.h"
+
+int test_push(void) {
+	return -1;
+}
+
+int test_push_three(void) {
+	return -1;
+}
+
+int test_push_nomem(void) {
+	/* How do you test malloc failure? */
+	return -1;
+}
+
+int test_pop(void) {
+	return -1;
+}
+
+int test_pop_two_of_three(void) {
+	return -1;
+}
+
+int test_pop_empty(void) {
+	return -1;
+}
+
+int test_delete_first(void) {
+	return -1;
+}
+
+int test_delete_middle(void) {
+	return -1;
+}
+
+int test_delete_last(void) {
+	return -1;
+}
+
+int test_delete_empty(void) {
+	return -1;
+}
+
+int test_delete_missing_book(void) {
+	return -1;
+}
+
+int test_find(void) {
+	return -1;
+}
+
+int test_find_empty(void) {
+	return -1;
+}
+
+int test_find_missing_book(void) {
+	return -1;
+}
+
+int test_sort(void) {
+	return -1;
+}
+
+int test_sort_one(void) {
+	return -1;
+}
+
+int test_sort_empty(void) {
+	return -1;
+}
+
+struct test_fn {
+	int (*fn_ptr)(void);
+	char * name;
+};
+
+/* Struct populated with :r!grep -o "test_[a-zA-Z_]*" tests.c | awk '{print "{ &"$0", \""$0"\" },"}' */
+struct test_fn tests[] = {
+	{ &test_push, "test_push" },
+	{ &test_push_three, "test_push_three" },
+	{ &test_push_nomem, "test_push_nomem" },
+	{ &test_pop, "test_pop" },
+	{ &test_pop_two_of_three, "test_pop_two_of_three" },
+	{ &test_pop_empty, "test_pop_empty" },
+	{ &test_delete_first, "test_delete_first" },
+	{ &test_delete_middle, "test_delete_middle" },
+	{ &test_delete_last, "test_delete_last" },
+	{ &test_delete_empty, "test_delete_empty" },
+	{ &test_delete_missing_book, "test_delete_missing_book" },
+	{ &test_find, "test_find" },
+	{ &test_find_empty, "test_find_empty" },
+	{ &test_find_missing_book, "test_find_missing_book" },
+	{ &test_sort, "test_sort" },
+	{ &test_sort_one, "test_sort_one" },
+	{ &test_sort_empty, "test_sort_empty" }
+};
+
+int main(void) {
+	unsigned int num_tests = sizeof(tests)/sizeof(tests[0]);
+	unsigned int ii = 0;
+	int ret_val = 0;
+
+	for ( ; ii < num_tests; ii++) {
+		ret_val = (*tests[ii].fn_ptr)();
+
+		if (0 == ret_val) {
+			printf("passed %s\n", tests[ii].name);
+		}
+		else {
+			printf("failed %s\n", tests[ii].name);
+		}
+	}
+	return 0;
+}
