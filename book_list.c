81f9bf46d8ece1b5879b64366083b2ac005998ba
athabasca <athabasca@users.noreply.github.com>
2022-02-07 14:29:17 -0800

    Add function definition stubs

---
+++ book_list.c
@@ -0,0 +1,9 @@
+#include "book_list.h"
+
+
+int push(node_t * head, const char * title, const char * author){ return -1; }
+int pop(node_t ** head, book_t * return_data){ return -1; }
+int delete(node_t ** head, const char * title){ return -1; }
+int find(node_t * head, const char * title, book_t * return_data){ return -1; }
+int sort(node_t * head, unsigned int flags){ return -1; }
+
