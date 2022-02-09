eb6c3bf84b85c646cf5c9a2b789b55613f1689b9
athabasca <athabasca@users.noreply.github.com>
2022-02-09 15:57:18 -0800

    Add find_index function to book_list and rename find to find_title

--- book_list.c
+++ book_list.c
@@ -4,6 +4,7 @@
 int push(node_t * head, const char * title, const char * author){ return -1; }
 int pop(node_t ** head, book_t * return_data){ return -1; }
 int delete(node_t ** head, const char * title){ return -1; }
-int find(node_t * head, const char * title, book_t * return_data){ return -1; }
+int find_title(node_t * head, const char * title, book_t * return_data){ return -1; }
+int find_index(node_t * head, const unsigned int index, book_t * return_data) { return -1; }
 int sort(node_t * head, unsigned int flags){ return -1; }
 
