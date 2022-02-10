d253f02b0b50ec5578d8c9065074cea51abe3cf8
athabasca <athabasca@users.noreply.github.com>
2022-02-09 17:17:20 -0800

    Implement push. It really bugs me that the tests for push use find as well, but I don't know how else to automate it

--- book_list.c
+++ book_list.c
@@ -1,7 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 #include "book_list.h"
 
 
-int push(node_t * head, const char * title, const char * author){ return -1; }
+int push(node_t * head, const char * title, const char * author) { 
+	book_t * data = NULL;
+	node_t * new_node = NULL;
+
+	data = malloc(sizeof(book_t));
+	if (NULL == data) {
+		return NO_MEMORY;
+	}
+
+	snprintf(data->title, strlen(title), "%s", title);
+	snprintf(data->author, strlen(author), "%s", author);
+
+	new_node = malloc(sizeof(node_t));
+	if (NULL == new_node) {
+		return NO_MEMORY;
+	}
+	new_node->data = data;
+
+	if (NULL == head) {
+		new_node->next = NULL;
+		head = new_node;
+	}
+	else {
+		new_node->next = head->next;
+		head->next = new_node;
+	}
+	return 0;
+}
+
+
 int pop(node_t ** head, book_t * return_data){ return -1; }
 int delete(node_t ** head, const char * title){ return -1; }
 int find_title(node_t * head, const char * title, book_t * return_data){ return -1; }
